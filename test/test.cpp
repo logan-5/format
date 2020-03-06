d7c919c2f07f8969fff846a427b1d574531a0a86
logan <logan.r.smith0@gmail.com>
2020-03-06 15:06:54 -0800

    remove profiling garbage from test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -1,10 +1,4 @@
-// #define PROFILING
-
-#ifdef PROFILING
-#define CATCH_CONFIG_RUNNER
-#else
 #define CATCH_CONFIG_MAIN
-#endif
 
 #include <catch.hpp>
 
@@ -25,25 +19,3 @@
 #include "format.hpp"
 
 #include "format.hpp"
-
-#include <iostream>
-
-#ifdef PROFILING
-template <class T>
-void doNotOptimizeAway(T&& datum) {
-    asm volatile("" : "+r"(datum));
-}
-
-int main() {
-	format(("{0}-{{"), 8);
-    for (;;) {
-        std::array<char, 24> data{};
-        doNotOptimizeAway(lrstd::format_to(data.data(), "{}", 42) -
-                          data.data());
-        //		std::cout << lrstd::format_to(data.data(), "{}", 42) -
-        //		data.data();
-        //		doNotOptimizeAway(lrstd::format(data.data(), "{}",
-        // 42).size());
-    }
-}
-#endif
